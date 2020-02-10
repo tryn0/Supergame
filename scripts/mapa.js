2cf3ad2e62f49f4fdd8084ac08433c699f32cef2
tryn0 <vkae@live.com>
2020-02-10 20:51:16 +0100

    functionality updated, redirect from earth to japan map when click on japans marker

--- scripts/mapa.js
+++ scripts/mapa.js
@@ -1,21 +1,24 @@
+import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r113/build/three.module.js';
+import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r113/examples/jsm/controls/OrbitControls.js';
+
 $(document).ready(function(){
-    $('#mapa').css('display','none');
-    $('#boton').click(function(){
-       /* $('#mapa').css({
+
+    //NO SIRVE
+    /*$('#boton').click(function(){
+        $('#mapa').css({
             'display' : 'block',
             'position' : 'fixed',
             'background-color' : 'rgba(0,0,0,0.5)',
             'z-index' : '1',
             'width' : '100%',
             'height' : '100%'
-        });*/
+        });
         $('#mapa>p').css('z-index','5');
         $('#contenido').load('./principal.html');
-    });
-    $('#mapa>p').click(function(){
-        $('#mapa').css('display','none');
-    });
-    anime({
+    });*///NO SIRVE
+
+
+    /*anime({
         targets: '.line-drawing-demo .lines path',
         strokeDashoffset: [anime.setDashoffset, 0],
         easing: 'easeInOutSine',
@@ -23,5 +26,135 @@ $(document).ready(function(){
         delay: function(el, i) { return i * 250 },
         direction: 'alternate',
         loop: true
-      });
+    });*/
+
+    //Creacion de la escena y el entorno
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight, 1,10000);
+    var renderer = new THREE.WebGLRenderer({antialias: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    //camera.position.set(0.0, 1.5, 3.0);
+    camera.position.set(0, 0, 1000);
+
+    renderer = new THREE.WebGLRenderer({
+         antialias: true
+    });
+
+    //Controles de la cámara
+    var controls = new OrbitControls(camera, renderer.domElement);
+    controls.maxDistance = 1000;
+    controls.minDistance = 750;
+    
+
+    //Función para el redimensionamiento de la pantalla
+    $(window).resize(onResize);
+    onResize();
+    function onResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    //Agregado del entorno de threejs al body
+    $('body').append(renderer.domElement);
+
+    //Creacion del objeto del mapa
+    var geometry = new THREE.BoxGeometry(1300, 1470, 25, 1, 1, 1);
+    var svg = new THREE.TextureLoader().load('../images/mapa_japon.png');
+    var material = new THREE.MeshBasicMaterial({
+        map: svg
+    });
+    var cube = new THREE.Mesh(geometry, material);
+
+    var niveles = [];
+
+    //Creacion de puntos
+    var tokio = new THREE.Mesh( new THREE.SphereGeometry(10, 50, 50 ), new THREE.MeshBasicMaterial( {color: 0xffff00} ) );
+    tokio.position.set(335,-15,25);
+    tokio.name = 'Tokio';
+
+    var hokkaido = new THREE.Mesh( new THREE.SphereGeometry(10, 50, 50 ), new THREE.MeshBasicMaterial( {color: 0xffff00} ) );
+    hokkaido.position.set(200,400,25);
+    hokkaido.name = 'Hokkaido';
+    hokkaido.color = 'grey';
+
+    var tokio2 = new THREE.Mesh( new THREE.SphereGeometry(10, 50, 50 ), new THREE.MeshBasicMaterial( {color: 0xffff00} ) );
+    tokio2.position.set(-335,-15,-25);
+    tokio2.name = 'Tokio';
+
+    var hokkaido2 = new THREE.Mesh( new THREE.SphereGeometry(10, 50, 50 ), new THREE.MeshBasicMaterial( {color: 0xffff00} ) );
+    hokkaido2.position.set(-200,400,-25);
+    hokkaido2.name = 'Hokkaido';
+
+    //Añadir puntos a lista para luego poder hacer click con raycaster
+    niveles.push(tokio, tokio2, hokkaido, hokkaido2);
+
+    //Añadir puntos a la escena (pantalla)
+    scene.add(cube,tokio,hokkaido, tokio2, hokkaido2);
+
+
+    //Creacion de líneas, cara1 (de frente)
+    var lineGeometry = new THREE.Geometry();
+    lineGeometry.vertices.push( new THREE.Vector3(335,-15,26), new THREE.Vector3(200,400,26) );
+    var lineMaterial = new THREE.LineBasicMaterial( { color: 'red',linewidth: 5 } );
+    var line = new THREE.Line( lineGeometry, lineMaterial );
+    scene.add(line);
+
+    //Creacion de líneas, cara2 (de espalda)
+    var lineGeometry2 = new THREE.Geometry();
+    lineGeometry2.vertices.push( new THREE.Vector3(-335,-15,-26), new THREE.Vector3(-200,400,-26) );
+    var lineMaterial2 = new THREE.LineBasicMaterial( { color: 'red',linewidth: 5 } );
+    var line2 = new THREE.Line( lineGeometry2, lineMaterial2 );
+    scene.add(line2);
+
+    
+    //Funcion que renderiza 60 veces por segundo la scene y camera, la pantalla vaya, 60fps
+    function render() {
+        requestAnimationFrame(render);
+        renderer.render(scene, camera);
+    }
+    render();
+
+
+    //Comprueba al hacer click si lo que has clicado está en la lista niveles
+    //si está comprueba el nombre del objeto, y según el nombre te redirecciona a un nivel u otro
+    $(window).click(function (event) {
+        // the following line would stop any other event handler from firing
+        // (such as the mouse's TrackballControls)
+        // event.preventDefault();
+
+        const rect = renderer.domElement.getBoundingClientRect();
+        const left = event.clientX - rect.left;
+        const top = event.clientY - rect.top;
+
+        const x = (left / rect.width) * 2 - 1;
+        const y = -(top / rect.height) * 2 + 1;
+
+        const raycaster = new THREE.Raycaster();
+        raycaster.ray.origin.setFromMatrixPosition(camera.matrixWorld);
+        raycaster.ray.direction.set(x, y, 0.5).unproject(camera).sub(raycaster.ray.origin).normalize();
+
+        const intersects = raycaster.intersectObjects(niveles, true);
+        if (intersects.length > 0){
+            switch(intersects[0].object.name){
+                //Si su nombre es tokio
+                case 'Tokio':
+                    if (confirm("¿Quieres viajar a "+intersects[0].object.name+"?")) {
+                        window.location.href = './tokio.html';
+                    }else{
+                        return false;
+                    }
+                  break;
+                //Si su nombre es hokkaido
+                case 'Hokkaido':
+                    if (confirm("¿Quieres viajar a "+intersects[0].object.name+"?")) {
+                        window.location.href = './hokkaido.html';
+                    }else{
+                        return false;
+                    }
+                    break;
+            }
+        }
+    });
 })
--- scripts/principal.js
+++ scripts/principal.js
@@ -1,7 +1,25 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r113/build/three.module.js';
 import { OrbitControls } from 'https://threejsfundamentals.org/threejs/resources/threejs/r113/examples/jsm/controls/OrbitControls.js';
 
-$(document).ready(function () {
+$('body').append('<div style="999" class="demo-content align-center line-drawing-demo cargando"><svg viewBox="0 0 280 100" width="200px" color="red"><g fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="1" class="lines"><path class="el" d="M58 80V50.12C57.7 41.6 51.14 35 43 35a15 15 0 0 0 0 30h7.5v15H43a30 30 0 1 1 0-60c16.42 0 29.5 13.23 30 29.89V80H58z"/><path class="el" d="M73 80V20H58v60h15z"/><path class="el" d="M58 80V49.77C58.5 33.23 71.58 20 88 20a30 30 0 0 1 30 30v30h-15V50a15 15 0 0 0-15-15c-8.14 0-14.7 6.6-15 15.12V80H58zm75 0V20h-15v60h15z"/><path class="el" d="M118 80V49.77C118.5 33.23 131.58 20 148 20a30 30 0 0 1 30 30v30h-15V50a15 15 0 0 0-15-15c-8.14 0-14.7 6.6-15 15.12V80h-15zm-7.5-60a7.5 7.5 0 1 1-7.48 8v-1c.25-3.9 3.5-7 7.48-7z"/><path class="el" d="M133 65a15 15 0 0 1-15-15v-7.5h-15V50a30 30 0 0 0 30 30V65zm30 15V49.77C163.5 33.23 176.58 20 193 20a30 30 0 0 1 30 30v30h-15V50a15 15 0 0 0-15-15c-8.14 0-14.7 6.6-15 15.12V80h-15z"/><path class="el" d="M238 65a15 15 0 0 1 0-30c8.1 0 14.63 6.53 15 15h-15v15h30V49.89C267.5 33.23 254.42 20 238 20a30 30 0 0 0 0 60V65z"/><path class="el" d="M260.48 65a7.5 7.5 0 1 1-7.48 8v-1c.26-3.9 3.5-7 7.48-7z"/></g></svg></div>');
+
+
+$(document).ready(function(){
+    /*FUNCIONA PARA LA ANIMACION DE PROGRSO CARGANDO
+    anime({
+        targets: '.line-drawing-demo .lines path',
+        strokeDashoffset: [anime.setDashoffset, 0],
+        easing: 'easeInOutSine',
+        duration: 1500,
+        delay: function(el, i) { return i * 250 },
+        direction: 'alternate',
+        loop: true
+    });*/
+    
+
+    
+    //Funcion para convertir latitud y longitud en Vector3
+    //necesario para color los marcadores e infobox en una esfera
     function _convertLatLonToVec3(lat, lon) {
         lat = lat * Math.PI / 180.0;
         lon = -lon * Math.PI / 180.0;
@@ -11,7 +29,7 @@ $(document).ready(function () {
             Math.cos(lat) * Math.sin(lon));
     }
 
-    function InfoBox(city, radius, domElement) {
+    /*function InfoBox(city, radius, domElement) {
         this._screenVector = new THREE.Vector3(0, 0, 0);
 
         this.position = _convertLatLonToVec3(city.lat, city.lng).multiplyScalar(radius);
@@ -38,9 +56,10 @@ $(document).ready(function () {
         // update the box overlays position
         this.box.style.left = (posx - boundingRect.width) + 'px';
         this.box.style.top = posy + 'px';
-    };
+    };*/
 
-    //--------------------------------
+    // -------------------------------------------------------------------
+    //Funcion para crear un marcador (chincheta)
     function Marker() {
         THREE.Object3D.call(this);
 
@@ -64,7 +83,7 @@ $(document).ready(function () {
     Marker.prototype = Object.create(THREE.Object3D.prototype);
 
     // ------ Earth object -------------------------------------------------
-
+    //Funcion para crear un objecto Earth (esfera, planneta tierra)
     function Earth(radius, texture, altura, agua) {
         THREE.Object3D.call(this);
 
@@ -109,23 +128,20 @@ $(document).ready(function () {
     init();
 
     function init() {
+        //Creacion de la scene, camera y entorno de threejs
         scene = new THREE.Scene();
-
         camera = new THREE.PerspectiveCamera(45, 4 / 3, 0.1, 100);
-        //camera.position.set(0.0, 1.5, 3.0);
         camera.position.set(-1.60, 1.5, -1.3);
-
         renderer = new THREE.WebGLRenderer({
             antialias: true
         });
 
+        //Creacion del control de la cámara
         controls = new OrbitControls(camera, renderer.domElement);
         controls.maxDistance = 4;
         controls.minDistance = 2;
 
-        /*var ambient = new THREE.AmbientLight(0xffffff, 0.5);
-        scene.add(ambient);*/
-
+        //Creacion de iluminacion
         var spotLight = new THREE.SpotLight(0xffffff);
         spotLight.position.set(-350, 0, -200);
         spotLight.castShadow = false; //If set to true light will cast dynamic shadows. Warning: This is expensive and requires tweaking to get shadows looking right.
@@ -150,10 +166,12 @@ $(document).ready(function () {
         //Añadir marcador
         earth.createMarker(35.683333, 139.683333, 'Tokio'); // Tokyo
 
+        //Añadir planeta a la scene
         scene.add(earth);
 
         //-------------
         // globe
+        //Creacion de infobox
 
         //var radius1 = 1;
 
@@ -199,11 +217,10 @@ $(document).ready(function () {
         //markerarry.push(marker2)
 
         //----------------
-        $(window).click(function (event) {
-            // the following line would stop any other event handler from firing
-            // (such as the mouse's TrackballControls)
-            // event.preventDefault();
-
+        //Funcion para comprobar si lo que se ha clicado está en la lista de chinchetas/marcadores
+        //si esta, en este caso como solo se ha programado japon, pues te lleva a japon
+        //pero en el código de japon se hizo para mas de 1 objeto
+        $(window).click(function (event){
             const rect = renderer.domElement.getBoundingClientRect();
             const left = event.clientX - rect.left;
             const top = event.clientY - rect.top;
@@ -217,40 +234,40 @@ $(document).ready(function () {
 
             const intersects = raycaster.intersectObjects(marcadores, true);
             if (intersects.length > 0) {
-                //alert(intersects[0].object.parent.name);
-                //console.log(intersects[0].object.parent.name);
                 if (confirm("¿Quieres viajar a Japón?")) {
                     window.location.href = './mapa.html';
                 } else {
                     return false;
                 }
             }
-
-        })
+        });
         //-----------------------
+        //Llamada a la funcion para el redimensionamiento de la ventana
         $(window).resize(onResize);
         onResize();
 
+        //Agregado del entorno threejs al body, lo agrega como canvas
         $('body').append(renderer.domElement);
 
         animate();
-
     }
 
+    //Funcion del redimensionamiento de la ventana
     function onResize() {
         camera.aspect = window.innerWidth / window.innerHeight;
         camera.updateProjectionMatrix();
-
         renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
-    function animate() {
-        //  label.update();
-
+    //Funcion que se llama asi misma y actualiza los controles de la cámara y renderiza la scene y la camera 60 veces por segundo, 60fps
+    function animate(){
         requestAnimationFrame(animate);
-
         controls.update();
-
         renderer.render(scene, camera);
     }
+    
+    /*TEMA DE LA PARTE DE PANTALLA DE CARGA
+    $('canvas').ready(function(){
+        $('svg').fadeOut('slow');
+    });*/
 })
